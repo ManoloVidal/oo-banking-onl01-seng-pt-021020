c5b78a026980e71bdf162f1fd623b0092fe8e289
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-03-11 19:37:07 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
+  attr_reader :name 
+  attr_accessor :balance, :status 
+  
+  def initialize(name)
+    @name = name 
+    @balance = 1000 
+    @status = "open"
+  end 
+  
+  def deposit(deposit_amount)
+    @balance += deposit_amount
+  end 
+  
+  def display_balance
+     "Your balance is $#{self.balance}."
+  end 
+  
+  def valid?(new_account)
+    new_account = BankAccount.new
+    if new_account.status = "open" && new_account.balance > 0 
+      true 
+    else
+      false 
+    end 
+  end 
 
 end
