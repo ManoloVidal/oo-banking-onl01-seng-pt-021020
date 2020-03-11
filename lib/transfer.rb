1474fe3b7b5c600be91a1823c3c2c0df6a27a42e
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-03-11 19:51:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
-  attr_reader :status
-  attr_accessor :sender, :receiver
+  attr_accessor :status
+  attr_reader :sender, :receiver
   
   def initialize(sender, receiver, status = "pending", amount = 50)
     @sender = sender
