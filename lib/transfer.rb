e5ac3e17307bc4d3b21b38131a03b0d0886f8f17
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-03-11 20:04:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,10 +13,18 @@ class Transfer
     sender.valid? && receiver.valid?
   end 
   
-  
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
     
     
-  end 
     
   end 
   
